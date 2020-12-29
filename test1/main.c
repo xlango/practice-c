9ad8c4ed866c4ad207d5b62d4646c4f122be3278
xhyl <1329172872@qq.com>
2020-12-28 21:24:00 -0500

    .c .o .h gdb

---
+++ test1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// #include "libc/max.h"
+// #include "libc/min.h"
+
+int main()
+{
+    int maxNum = max(1,2);
+    int minNum = min(3,4);
+    printf("%d\n%d",maxNum,minNum);
+
+    return 0;
+}
---
+++ test1/min.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int min(int a,int b)
+{
+    if(a<b){
+        return a;
+    }
+
+    return b;
+}
